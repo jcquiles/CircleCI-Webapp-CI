22750df83298c1c0171556de67bd34d08a29e8fc
Christopher Quiles <chris.quiles@evercast.us>
2021-10-25 21:44:45 -0700

    testing again on go file

--- main_test.go
+++ main_test.go
@@ -12,7 +12,7 @@ func TestHelloWorld(t *testing.T) {
 
 	helloWorld(response, request)
 	got := response.Body.String()
-	want := "Test CI Webserver 3"
+	want := "CI test webserver 6"
 
 	if got != want {
 		t.Error("Wanted:", want, "Got:", got)
